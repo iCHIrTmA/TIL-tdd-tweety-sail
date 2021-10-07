815bf5de6aec3a3050426fc95ba15d6fa40654ce
janroeNabepero <janroeNabepero@nabepero.co.jp>
2021-10-07 18:53:16 +0800

    slight tweak horizontal rule

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,16 +1,18 @@
-    <div class="card-body">
-        <div class="row d-flex align-items-center">
-            <img src="{{ asset('images/default-avatar.jpg') }}" 
-            alt=""
-            class="rounded-circle mx-3"
-            style="max-width: 7%; height: fit-content";
-            >
-            <div>
-                <h3 class="mt-3" style="font-size: 1.0rem; font-weight: 700; color:black;">{{ $tweet->owner->name }}</h4>
-            </div>
+<div class="card-body">
+    <div class="row d-flex align-items-center">
+        <img src="{{ asset('images/default-avatar.jpg') }}" 
+        alt=""
+        class="rounded-circle mx-3"
+        style="max-width: 7%; height: fit-content";
+        >
+        <div>
+            <h3 class="mt-3" style="font-size: 1.0rem; font-weight: 700; color:black;">{{ $tweet->owner->name }}</h4>
         </div>
-        <p class="card-text mt-2" style="font-size: 1.0rem; font-weight: 500; color:black;">
-            {{ $tweet->body }}
-        </p>
     </div>
+    <p class="card-text mt-2" style="font-size: 1.0rem; font-weight: 500; color:black;">
+        {{ $tweet->body }}
+    </p>
+</div>
+@unless($loop->last)
     <hr>
+@endunless
