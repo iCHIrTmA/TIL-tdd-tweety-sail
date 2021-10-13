dc9d0e150f5d4d81987b970a7e26f00e5e2524d3
janroeNabepero <janroeNabepero@nabepero.co.jp>
2021-10-13 11:47:11 +0800

    added user hasMany tweets rel and pivot user belongsToMany users rel test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -3,15 +3,18 @@
 namespace Tests\Unit;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
-    * @test
-    */
+     * @test
+     */
     public function userCanFollowOtherUser(): void
     {
         $this->withoutExceptionHandling();
@@ -22,20 +25,36 @@ class UserTest extends TestCase
         $third_user = User::factory()->create(['name' => 'third_user']);
 
         // make first user follow the second user
-
         $first_user->follow($second_user);
-
-        // assert belongsToMany relationship
-        // $this->assertR(1, $first_user->follows);
 
         // assert follows relationship
         $this->assertCount(1, $first_user->follows);
-
 
         // follow another user
         // TODO: use tap() for fresh()
         $first_user->follow($third_user);
 
         $this->assertCount(2, $first_user->fresh()->follows);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_belongs_to_many_user_through_pivot_table(): void
+    {
+        $user = User::factory()->create();
+
+        $this->assertInstanceOf(BelongsToMany::class, $user->follows());
+        // TODO: assert pivot table
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_has_many_tweets(): void
+    {
+        $user = User::factory()->create();
+        
+        $this->assertInstanceOf(HasMany::class, $user->tweets());
     }
 }
