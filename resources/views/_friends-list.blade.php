bb1fdae4182b23790e58a25788e752519f83685a
janroeNabepero <janroeNabepero@nabepero.co.jp>
2021-10-14 14:26:08 +0800

    added username in registration and layout for profile edit page

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,6 +1,6 @@
 <h3 class="my-4" style="font-size: 1.2rem; font-weight: 700; color:black;">Friends</h3>
 <ul class="list-group">
-    @foreach(current_user()->follows as $user)
+    @forelse(current_user()->follows as $user)
         <li class="list-unstyled mb-2" style="font-size: 1rem; font-weight: 700;">
             <a href="{{ route('profiles.show', $user) }}">
                 <img src="{{ asset('images/default-avatar.jpg') }}" 
@@ -11,5 +11,9 @@
                 {{ $user->name }}
             </a>
         </li>
-    @endforeach
+    @empty
+        <li class="list-unstyled mb-2" style="font-size: 1rem; font-weight: 700;">
+            No friends yet
+        </li>
+    @endforelse
 </ul>
--- resources/views/_timeline.blade.php
+++ resources/views/_timeline.blade.php
@@ -1,5 +1,7 @@
 <div class="card mt-4" style="border: 1px solid #d9d2d2; border-radius: 1.25rem;">
-    @foreach($tweets as $tweet)
+    @forelse($tweets as $tweet)
         @include('_tweet')
-    @endforeach
+    @empty
+        <p class="p-4">No tweets yet</p>
+    @endforelse
 </div>
